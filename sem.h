ea3c0ac31d8195171588bfd895e6d425e966f1bf
Nathaniel Lozano <43077766+nathaniellozano@users.noreply.github.com>
2019-12-03 15:31:53 -0600

    Update sem.h

--- sem.h
+++ sem.h
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+// struct for semaphore 
+// will be used for signalling and waiting
 typedef struct {
   int fd[2];
 } Semaphore;
-
+//functions used in sem.c
 void sem_init(Semaphore *s, int value);
 void sem_close(Semaphore *s);
 void sem_wait(Semaphore *s);
